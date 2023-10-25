a39a5a7d2e257e4b6b9fa0936238dc3d6b35b5e6
Addika1630 <addika1630@gmail.com>
2023-10-25 17:08:11 +0000

    Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Create a class LIFOCache that inherits from
+"""Create a class LRUCache that inherits from
 BaseCaching and is a caching system:
 
 You must use self.cache_data - dictionary from
@@ -13,10 +13,10 @@ If key or item is None, this method should not do
 anything.
 If the number of items in self.cache_data is higher
 that BaseCaching.MAX_ITEMS:
-you must discard the last item put in cache
-(LIFO algorithm)
-you must print DISCARD: with the key discarded and
-following by a new line
+you must discard the least recently used item
+(LRU algorithm)
+you must print DISCARD: with the key discarded
+and following by a new line
 def get(self, key):
 Must return the value in self.cache_data linked to key.
 If key is None or if the key doesn’t exist in
@@ -27,7 +27,7 @@ self.cache_data, return None.
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LIFOCache(BaseCaching):
+class LRUCache(BaseCaching):
     """_summary_
     """
 
@@ -35,6 +35,7 @@ class LIFOCache(BaseCaching):
         """_summary_
         """
         super().__init__()
+        self.usedKeys = []
 
     def put(self, key, item):
         """_summary_
@@ -43,16 +44,17 @@ class LIFOCache(BaseCaching):
                         key (_type_): _description_
                         item (_type_): _description_
         """
-        if key is None or item is None:
-            pass
-        else:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data.keys():
-                # delete the last item in the dictionary
-                last_key, last_value = self.cache_data.popitem()
-                print("DISCARD: {}". format(last_key))
-
+        if key is not None and item is not None:
             self.cache_data[key] = item
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
         """return the value in self.cache_data linked to key
@@ -60,6 +62,7 @@ class LIFOCache(BaseCaching):
         Args:
                         key (_type_): _description_
         """
-        if key is None or key not in self.cache_data.keys():
-            return None
-        return self.cache_data.get(key)
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
+        return None
